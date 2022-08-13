a64eb560bb79bae1bbf1b535068be305fd21d8f2
Anna Sokolowska <milanka.anka@gmail.com>
2022-08-13 18:51:59 +0200

    Lesson3dop

--- modules/burger.js
+++ modules/burger.js
@@ -1,6 +1,22 @@
 const burger = document.querySelector('.header__menu-button');
 const burgerMenu = document.querySelector('.header__menu');
+const items = document.querySelectorAll('.header__item');
 
 burger.addEventListener('click', () => {
   burgerMenu.classList.toggle('header__menu_active');
+  document.addEventListener('click', e => {
+    const target = e.target;
+    if ((target !== burgerMenu) && (target !== burger)) {
+      burgerMenu.classList.remove('header__menu_active');
+    }
+  });
 });
+
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    burgerMenu.classList.toggle('header__menu_active');
+  });
+});
+
+
